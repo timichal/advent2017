a2e8cb26d9c4e72da869aeab34c4d2e8e067cf32
timichal <michal@zlatkovsky.cz>
2017-12-03 15:01:25 +0100

    refactored day 3

--- day03.py
+++ day03.py
@@ -1,14 +1,11 @@
-def init_grid(size):
-	return [[0] * size for i in range(size)] 
+from collections import defaultdict
 
 # part 1
-def fill_grid_part1(target):
-	size = round(target**0.5)
-	grid = init_grid(size)
-	pos = [int(size/2), int(size/2)]
-	center = pos
+def grid_part1(target):
+	grid = {}
+	pos = [0, 0]
 	number = 1
-	grid[pos[0]][pos[1]] = number
+	grid[(0,0)] = number
 	# right 1, up 1, left 2, down 2, right 3, up 3...
 	counter = 1
 	# 1: right, 2: up, 3: left, 4: down, 5: right...
@@ -16,51 +13,50 @@ def fill_grid_part1(target):
 	while True:
 		for times in range(counter):
 			number += 1
-			if direction % 4 == 1: pos = [pos[0], pos[1]+1]
-			elif direction % 4 == 2: pos = [pos[0]-1, pos[1]]
-			elif direction % 4 == 3: pos = [pos[0], pos[1]-1]
-			elif direction % 4 == 0: pos = [pos[0]+1, pos[1]]
+			if direction % 4 == 1: pos[1] += 1
+			elif direction % 4 == 2: pos[0] -= 1
+			elif direction % 4 == 3: pos[1] -= 1
+			elif direction % 4 == 0: pos[0] += 1
 
-			grid[pos[0]][pos[1]] = number
+			grid[(pos[0], pos[1])] = number
 			if number == target:
-				return abs(pos[0]-center[0]) + abs(pos[1]-center[0])
+				return abs(pos[0]) + abs(pos[1])
 
 		if direction % 2 == 0: counter += 1
 		direction += 1
 
 # part 2
-def fill_grid_part2(size, target):
+def grid_part2(target):
 	def getvalue(pos):
-		return grid[pos[0]+1][pos[1]] +\
-			   grid[pos[0]-1][pos[1]] +\
-			   grid[pos[0]][pos[1]+1] +\
-			   grid[pos[0]][pos[1]-1] +\
-			   grid[pos[0]+1][pos[1]+1] +\
-			   grid[pos[0]+1][pos[1]-1] +\
-			   grid[pos[0]-1][pos[1]+1] +\
-			   grid[pos[0]-1][pos[1]-1]
+		return grid[(pos[0]+1, pos[1])] +\
+			   grid[(pos[0]-1, pos[1])] +\
+			   grid[(pos[0], pos[1]+1)] +\
+			   grid[(pos[0], pos[1]-1)] +\
+			   grid[(pos[0]+1, pos[1]+1)] +\
+			   grid[(pos[0]+1, pos[1]-1)] +\
+			   grid[(pos[0]-1, pos[1]+1)] +\
+			   grid[(pos[0]-1, pos[1]-1)]
 
-	grid = init_grid(size)
-	pos = [int(size/2), int(size/2)]
-	center = pos
-	grid[pos[0]][pos[1]] = 1
+	grid = defaultdict(int)
+	pos = [0, 0]
+	grid[(0, 0)] = 1
 	# right 1, up 1, left 2, down 2, right 3, up 3...
 	counter = 1
 	# 1: right, 2: up, 3: left, 4: down, 5: right...
 	direction = 1
 	while True:
 		for times in range(counter):
-			if direction % 4 == 1: pos = [pos[0], pos[1]+1]
-			elif direction % 4 == 2: pos = [pos[0]-1, pos[1]]
-			elif direction % 4 == 3: pos = [pos[0], pos[1]-1]
-			elif direction % 4 == 0: pos = [pos[0]+1, pos[1]]
+			if direction % 4 == 1: pos[1] += 1
+			elif direction % 4 == 2: pos[0] -= 1
+			elif direction % 4 == 3: pos[1] -= 1
+			elif direction % 4 == 0: pos[0] += 1
 
 			if getvalue(pos) > target: return getvalue(pos)
-			grid[pos[0]][pos[1]] = getvalue(pos)	
-			
+			grid[(pos[0],pos[1])] = getvalue(pos)	
+
 		if direction % 2 == 0: counter += 1
 		direction += 1
 
 n = 312051
-print("Part 1:", fill_grid_part1(n))
-print("Part 2:", fill_grid_part2(11,n))
+print("Part 1:", grid_part1(n))
+print("Part 2:", grid_part2(n))
