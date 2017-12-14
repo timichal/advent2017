e805c905052c72612bd6c255cca91e1941ea1aa6
timichal <michal@zlatkovsky.cz>
2017-12-14 13:23:05 +0100

    refactored day 13

--- day13.py
+++ day13.py
@@ -1,47 +1,39 @@
-inp = {}
+firewall = {}
 with open("day13") as file:
     for line in file:
-        inp[int(line.split()[0][:-1])] = int(line.split()[1])
+        firewall[int(line.split()[0][:-1])] = (1, int(line.split()[1]))
 
-deffirewall = {}
-for size in range(max(inp)+1):
-    if size in inp:
-        # pos, length
-        deffirewall[size] = (1, inp[size])
-    else:
-        deffirewall[size] = 0
+for position in range(max(firewall)):    
+    if position not in firewall:
+        firewall[position] = 0
 
-def move(firewall, steps):
-    for i, layer in firewall.items():
-        if layer != 0:
-            pos = steps % (layer[1]*2-2)
-            if pos < layer[1]:
-                firewall[i] = (layer[0] + pos, layer[1])
-            elif pos >= layer[1]:
-                firewall[i] = (layer[1] - (pos - (layer[1] - 1)), layer[1])
-    return firewall
+def movelayer(layer, steps):
+    pos = steps % (layer[1]*2-2)
+    if pos < layer[1]:
+        layer = (layer[0] + pos, layer[1])
+    elif pos >= layer[1]:
+        layer = (layer[1] - (pos - (layer[1] - 1)), layer[1])        
+    return layer
 
-i=0
+# part 1
+severity = 0
+for i, layer in firewall.items():
+    if layer != 0:
+        layer = movelayer(layer, i)
+        if layer[0] == 1:
+                severity += i*layer[1]
+print("Part 1: severity " + str(severity))
+
+# part 2
+i = 0
 while True:
-        severity = 0
-        passed = True
-        for j in range(len(deffirewall)):
-            firewall = move(deffirewall.copy(), i+j)
-            if firewall[j] != 0:
-                if firewall[j][0] == 1:
-                    passed = False
-                    if i == 0:
-                        severity += j*firewall[j][1]
-                    else:
-                        break
-                    
-        
-        if i == 0:
-            print("Part 1: severity " + str(severity))
-            pass
-        
-
-        if passed:
-            print("Part 2: " + str(i))
-            break
-        i+=1
+    passed = True
+    for j, layer in firewall.items():
+        if layer != 0:
+            if movelayer(layer, i+j)[0] == 1:
+                passed = False
+                break
+    if passed:
+        print("Part 2: " + str(i))
+        break
+    i += 1
